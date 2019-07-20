e566fd51f0f672cc0733e4c9350817877e7fc969
jagpreetkb <jagpreetbhandaal@gmail.com>
2019-07-20 21:33:55 +0000

    code changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,26 @@ class PostsController < ApplicationController
   end
 
   def new 
+    @post = Post.new
   end
 
   def create 
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else 
+      render "new"
+    end
   end
+
+  def show
+    @post.find(params[:id])
+  end
+
+  private 
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end 
 end
